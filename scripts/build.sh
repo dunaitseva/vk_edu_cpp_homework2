761059fcc697dbed83664d8b30f197f6351cbad2
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-24 22:56:37 +0300

    Created build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+BINARY_DIR="build"
+CMAKE_OPTIONS=""
+while getopts "tp" opt; do
+    case $opt in
+    t)
+      CMAKE_OPTIONS="$CMAKE_OPTIONS -DTEST=ON "
+      ;;
+    p)
+      CMAKE_OPTIONS="$CMAKE_OPTIONS -DPARALLEL=ON "
+      ;;
+    a*)
+      echo "Unexpected flag $opt"
+      exit 1
+      ;;
+    esac
+done
+
+echo "Enabled cmake options: $CMAKE_OPTIONS"
+rm -rf $BINARY_DIR
+
+cmake $CMAKE_OPTIONS -S . -B $BINARY_DIR ||
+echo "Build ends with errors. Binary directory was not created." && exit 1
+
+echo "Binary directory was created with name $BINARY_DIR. Build finished success"
