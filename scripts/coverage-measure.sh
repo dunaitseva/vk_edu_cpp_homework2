26949b33dace2fbd64d1d07ca38832b4b3ce05c9
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-03-31 09:48:27 +0300

    Excluded delegated main function logic from cov

--- scripts/coverage-measure.sh
+++ scripts/coverage-measure.sh
@@ -7,7 +7,7 @@ pip install gcovr
 cd $BUILD_DIRECTORY || (echo "Error. $BUILD_DIRECTORY not created." && exit 1)
 ctest
 cd ..
-EXCLUDING_FILES=".*_deps.*|.*tests.*|project/main.c"
+EXCLUDING_FILES=".*_deps.*|.*tests.*|project/main.c|project/src/include.c"
 #EXCLUDING_FILES=".*_deps.*"
 REPORT_FILE_NAME="coverage.xml"
 gcovr -r . ./build -e $EXCLUDING_FILES -o $REPORT_FILE_NAME --xml
